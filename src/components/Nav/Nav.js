f2aad5e09b59f49c811867adac3a55017349bb22
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-06-25 16:17:47 -0500

    Revert "Fixed Navbar"
    This reverts commit 9ba6eb97284b78161e59f1101ab3162803b44081.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,31 +7,19 @@ class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      countries: [
-            {name: "Tanzania"},
-            {name: "Kenya"},
-            {name: "Uganda"},
-            {name: "Rwanda"},
-            {name: "Burundi"},
-            {name: "Democratic Republic of the Congo"},
-            {name: "Malawi"},
-            {name: "South Sudan"}
-          ]
     }
   }
 
+
+
   render() {
-    const {name} = this.state.countries
-    console.log(this.state.countries);
     return(
       <div className="boardNav">
         <Link to="/">  <h3> Menu </h3> </Link>
+
         <div>
-          {this.state.countries.map(countries =>(
-            <li key={countries.name}>
-              <Link className="countries" to={`/country/${countries.name}`}> {countries.name} </Link>
-            </li>
-          ))}
+            <Link to="/countries"> countries </Link>
+            <Link to="/topics"> Topics </Link >
         </div>
       </div>
     )
